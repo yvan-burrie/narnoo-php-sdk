f98107f98ab0b1660eaf608a353c0789e1778e37
yvan-burrie <yvan.burrie@hotmail.com>
2020-11-24 22:36:38 +1000

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,20 @@ include "vendor/autoload.php";
 
 use Narnoo\Authenticate\Authenticate;
 
-//Narnoo developer keys
+// Narnoo developer keys
 $access = "";
 $secret = "";
 
-
-$token = Authenticate::getToken($access,$secret);
-if(!empty($token) && is_string($token)){
-	$data = $token;
-}else{
-	$data =  "No token returned";
+$token = Authenticate::getToken($access, $secret);
+if (!empty($token) && is_string($token))
+{
+    $data = $token;
 }
-
+else
+{
+    $data = "No token returned";
+}
 
 echo "<pre>";
 print_r($data);
 echo "</pre>";
-?>
